32f189f62e420224b19ed3034e35145dd30d3d90
Alexander <alexander@dce.bg>
2018-06-20 20:41:14 +0300

    Allowing 'demo' and 'keyboard_tutorial' apps to receive and process keyboard events

--- main.c
+++ main.c
@@ -7,15 +7,37 @@
 /*********************
  *      INCLUDES
  *********************/
+/* INCLUDE THIS FILE FIRST! IT SETS UP DEFINES AND SYSTEM SETTINGS */
+#include "lv_drv_conf.h"
+
+#include <assert.h>
+#include <features.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+#if defined _POSIX_SOURCE
 #include <unistd.h>
+#endif      /*_POSIX_SOURCE*/
+
 #include <SDL2/SDL.h>
+
 #include "lvgl/lvgl.h"
 #include "lv_drivers/display/monitor.h"
 #include "lv_drivers/indev/mouse.h"
+#if USE_KEYBOARD
+#include "lv_drivers/indev/keyboard.h"
+#endif
+
+#if BUILD_DEMO
 #include "lv_examples/lv_apps/demo/demo.h"
+#elif BUILD_TUTORIAL
+#include "lv_examples/lv_tutorial/10_keyboard/lv_tutorial_keyboard.h"
+#else
+/* TODO: Provide other switches to control these demo builds
 #include "lv_examples/lv_apps/benchmark/benchmark.h"
 #include "lv_examples/lv_tests/lv_test_theme/lv_test_theme.h"
+*/
+#endif
 
 /*********************
  *      DEFINES
@@ -40,6 +62,13 @@ static int tick_thread(void *data);
  **********************/
 
 /**********************
+ *   GLOBAL VARIABLES
+ **********************/
+#if USE_KEYBOARD
+lv_indev_t * g_kbd_dev = NULL;
+#endif
+
+/**********************
  *   GLOBAL FUNCTIONS
  **********************/
 
@@ -51,9 +80,13 @@ int main(int argc, char** argv)
     /*Initialize the HAL for LittlevGL*/
     hal_init();
 
+#if BUILD_DEMO
     /*Load a demo*/
-    demo_create();
-
+    demo_create(g_kbd_dev);
+#elif BUILD_TUTORIAL
+    /*Load keyboard tutorial code*/
+    lv_tutorial_keyboard(g_kbd_dev);
+#endif
     /*Or try the benchmark too to see the speed of your MCU*/
     //benchmark_create();
 
@@ -89,6 +122,16 @@ static void hal_init(void)
     disp_drv.disp_fill = monitor_fill;
     disp_drv.disp_map = monitor_map;
     lv_disp_drv_register(&disp_drv);
+
+#if USE_KEYBOARD
+    /* Use the 'keyboard' driver which reads the PC's keyboard*/
+    keyboard_init();
+    lv_indev_drv_t kbd_drv;
+    lv_indev_drv_init(&kbd_drv);          /*Basic initialization*/
+    kbd_drv.type = LV_INDEV_TYPE_KEYPAD;
+    kbd_drv.read = keyboard_read;         /*This function will be called periodically (by the library) to read the keyboard*/
+    g_kbd_dev = lv_indev_drv_register(&kbd_drv);
+#endif
 
     /* Add the mouse (or touchpad) as input device
      * Use the 'mouse' driver which reads the PC's mouse*/
