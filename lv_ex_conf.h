954bf275ba53181ad66193a4f37315d321b02e55
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-05-22 11:34:08 +0200

    submodule update

--- lv_ex_conf.h
+++ lv_ex_conf.h
@@ -10,16 +10,30 @@
 /*******************
  * GENERAL SETTING
  *******************/
-#define LV_EX_PRINTF       1       /*Enable printf-ing data*/
-#define LV_EX_KEYBOARD     1       /*Add PC keyboard support to some examples (`lv_drvers` repository is required)*/
+#define LV_EX_PRINTF       1       /*Enable printf-ing data in demoes and examples*/
+#define LV_EX_KEYBOARD     1       /*Add PC keyboard support to some examples (`lv_drivers` repository is required)*/
 #define LV_EX_MOUSEWHEEL   1       /*Add 'encoder' (mouse wheel) support to some examples (`lv_drivers` repository is required)*/
 
 /*********************
  * DEMO USAGE
  *********************/
 
-/*Widgets demo*/
+/*Show some widget*/
+#define LV_USE_DEMO_WIDGETS        1
+#if LV_USE_DEMO_WIDGETS
 #define LV_DEMO_WIDGETS_SLIDESHOW  0
+#endif
 
+/*Printer demo, optimized for 800x480*/
+#define LV_USE_DEMO_PRINTER     1
+
+/*Demonstrate the usage of encoder and keyboard*/
+#define LV_USE_DEMO_KEYPAD_AND_ENCODER     1
+
+/*Benchmark your system*/
+#define LV_USE_DEMO_BENCHMARK   1
+
+/*Stress test for LVGL*/
+#define LV_USE_DEMO_STRESS      1
 #endif /*LV_EX_CONF_H*/
 
