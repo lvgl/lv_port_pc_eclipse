5e2d41b018150b3b016d5c6450c505e1d67e0d5e
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-01-25 22:00:35 +0200

    Fixed build for main targets

--- cmake/declare_lvgl_target_library.cmake
+++ cmake/declare_lvgl_target_library.cmake
@@ -1,4 +1,4 @@
-function (lvgl_create_static_library_target TARGET_NAME GENERATED_FILES_DIR TARGET_LINK_EXT_LIBRARIES )
+function (lvgl_create_static_library_target TARGET_NAME GENERATED_FILES_DIR)
 
     set(LV_TARGET_SOURCES_PATH "${GENERATED_FILES_DIR}/${TARGET_NAME}_c.txt")
     set (LV_TARGET_HEADERS_PATH "${GENERATED_FILES_DIR}/${TARGET_NAME}_h.txt")
@@ -19,17 +19,17 @@ function (lvgl_create_static_library_target TARGET_NAME GENERATED_FILES_DIR TARG
     set (MODIFIED_INCLUDES_LIST "")
     foreach(INCLUDE_PATH ${C_LVGL_HEADERS} )
         string(REPLACE "./" "${TARGET_NAME}/" INCLUDE_OUT ${INCLUDE_PATH})
-        list(APPEND MODIFIED_INCLUDES_LIST ${INCLUDE_OUT})
+        get_filename_component(LV_INCLUDE_DIR ${INCLUDE_PATH} DIRECTORY)
+        list(APPEND MODIFIED_INCLUDES_LIST ${LV_INCLUDE_DIR})
     endforeach()
 
     set_source_files_properties(${MODIFIED_SOURCE_LIST} PROPERTIES LANGUAGE C)
     add_library( ${TARGET_NAME} STATIC ${MODIFIED_SOURCE_LIST})
-
-    target_sources(${TARGET_NAME} PUBLIC ${MODIFIED_INCLUDES_LIST} ${MODIFIED_SOURCE_LIST})
     if(WIN32)
-        target_compile_definitions(${TARGET_NAME} PRIVATE _CRT_SECURE_NO_WARNINGS)
+        target_compile_definitions(${TARGET_NAME} PUBLIC _CRT_SECURE_NO_WARNINGS)
     endif(WIN32)
 
-    
-    target_link_libraries( ${TARGET_NAME} PUBLIC ${TARGET_LINK_EXT_LIBRARIES})
+    list(REMOVE_DUPLICATES MODIFIED_INCLUDES_LIST)
+    list(APPEND MODIFIED_INCLUDES_LIST "${TARGET_NAME}")
+    target_include_directories(${TARGET_NAME} PUBLIC ${MODIFIED_INCLUDES_LIST} )
 endfunction()
