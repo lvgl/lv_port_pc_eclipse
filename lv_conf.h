56e7f595b23bb29d2fb26ba39c6378129118bfd0
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-10-14 10:09:26 +0200

    Update lv_conf.h

--- lv_conf.h
+++ lv_conf.h
@@ -846,7 +846,7 @@
 #define LV_USE_SNAPSHOT 0
 
 /*1: Enable system monitor component*/
-#define LV_USE_SYSMON   0
+#define LV_USE_SYSMON   1
 #if LV_USE_SYSMON
     /*Get the idle percentage. E.g. uint32_t my_get_idle(void);*/
     #define LV_SYSMON_GET_IDLE lv_timer_get_idle
